5133e99fe0c8a7eeb00d173700195e493c9230fe
Abdullah <ashiqueashique465@gmail.com>
2020-03-23 21:50:45 +0600

    let var const

---
+++ js/style.js
@@ -0,0 +1,20 @@
+function vara(){
+	for (var i = 0; i <10; i++) {
+		console.log("Loop er vitore vaire shob jaigai kaj korbe");
+	}
+}
+function lett(){
+	for(let i=1; i<4; i++){
+		console.log("just loop er vitore kaj korbe");
+	}
+}
+
+function constet(){
+	for(const i=1; i<4; i++){
+		console.log("valu 1st ja deya hoi tai thakbe r chnage hobe nah");
+	}
+}
+
+vara();
+lett();
+constet();
