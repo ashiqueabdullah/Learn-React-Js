b1b2b6bbac7361bf75749f3d7dfccff09827147e
Abdullah <ashiqueashique465@gmail.com>
2020-03-25 09:44:04 +0600

    component

---
+++ leran-react/src/component/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import First from './First/first';
+//First clsaa name, first file name
+
+function App() {
+  let p="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione est quam a ex aliquam quis tenetur quia laboriosam corporis vitae. Itaque illum modi culpa vel. Doloribus labore mollitia cupiditate amet";
+  return (
+    <div className="App">
+     <First />
+    </div> 
+  );
+}
+
+export default App;
---
+++ leran-react/src/component/First/first.js
@@ -0,0 +1,14 @@
+import React, {Component}  from 'react'
+
+class First extends Component{
+    render(){
+        return (
+            <div>
+                <h1>I am Ashique Abdullah</h1>
+                <p>Now i am learning React Js</p>
+            </div>
+        )
+    }
+}
+
+export default First
