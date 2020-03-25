0b36ffe6035063dbae8b8d8fe0b6884e4ea4ba06
Abdullah <ashiqueashique465@gmail.com>
2020-03-25 13:14:51 +0600

    state arrow function increase decrease

--- leran-react/src/component/App.js
+++ leran-react/src/component/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 import './App.css';
 import First from './First/first';
 //First clsaa name, first file name
+import Cont from './counter/count';
 
 function App() {
   let p="Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione est quam a ex aliquam quis tenetur quia laboriosam corporis vitae. Itaque illum modi culpa vel. Doloribus labore mollitia cupiditate amet";
   return (
     <div className="App">
      <First name="Ashique Abdullah" email="ashique@gmail.com" address="29 Rajar Golli"/>
-     <First name=" Abdullah" email="ashique@gmail.com" address="29 Rajar Golli"/>
+     <First name="Abdullah" email="ashique@gmail.com" address="29 Rajar Golli"/>
+     <Cont />
     </div> 
   );
 }
--- leran-react/src/component/First/first.js
+++ leran-react/src/component/First/first.js
@@ -12,4 +12,5 @@ class First extends Component{
     }
 }
 
+
 export default First
