8df8c44af4a7bdc5de81f123faa725dd040cc7b9
Abdullah <ashiqueashique465@gmail.com>
2020-03-26 10:52:39 +0600

    funtional component with props

---
+++ another react/learnreact/src/component/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import Test from './functional'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>I am Learning React</h1>
+      <Test name="Ashique Abdullah"></Test>
+    </div>
+  );
+}
+
+export default App;
---
+++ another react/learnreact/src/component/functional.js
@@ -0,0 +1,8 @@
+import React, {Component} from 'react'
+
+function Test(props){
+    return(
+    <h1>I am {props.name}</h1>
+    )
+}
+export default Test
