ddc92e34ca14ded6d0155c041a49d4d8ec228b70
Abdullah <ashiqueashique465@gmail.com>
2020-03-26 11:50:08 +0600

    function call using onClick atribute in class and function

--- another react/learnreact/src/component/classf.js
+++ another react/learnreact/src/component/classf.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react'
 
 class Classtest extends Component{
+     clcs(){
+        alert("Kaj korse");
+    }
     render(){
-        return <h2>I am {this.props.age} years old</h2>
-        
+        return(
+            <div>
+                <button onClick={this.clcs}>Button From Class</button>
+                <h2>I am {this.props.age} years old</h2>
+            </div>
+        )        
     }
 }
 export default Classtest
--- another react/learnreact/src/component/functional.js
+++ another react/learnreact/src/component/functional.js
@@ -1,8 +1,14 @@
 import React, {Component} from 'react'
 
 function Test(props){
+    function clc(){
+        alert("Click korci")
+    }
     return(
-    <h1>I am {props.name}</h1>
+        <div>
+            <button onClick={clc}>Button From function</button>
+            <h1>I am {props.name}</h1>
+        </div>
     )
 }
 export default Test
