70e9eefcb97dee1dd5bc6c584f219c4e516ef564
Abdullah <ashiqueashique465@gmail.com>
2020-03-26 21:33:49 +0600

    state create using object, array function in state

--- another react/learnreact/src/component/makeState.js
+++ another react/learnreact/src/component/makeState.js
@@ -3,15 +3,20 @@ import React, {Component} from 'react'
 class MakeState extends Component{
     constructor(){
         super()
-        this.state={
-            color:"red",
-            name:"Mango"
+        var obj={
+            color:["red","green"],
+            name:"Mango",
+            country:{
+                one:"Bangladesh",
+                two:"India"
+            }
         }
+        this.state=obj;
     }
     render(){
         return(
             <div>
-                <h1>Color {this.state.color}</h1>
+                <h1>{this.state.color[1]}Color {this.state.country.one} {this.state.name}</h1>
             </div>
         )
     }
