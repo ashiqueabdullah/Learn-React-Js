5bd1ccf80f655878b3ddac3d87926c8366c6cb8b
Abdullah <ashiqueashique465@gmail.com>
2020-03-26 11:05:42 +0600

    class component with props

--- another react/learnreact/src/component/App.js
+++ another react/learnreact/src/component/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import Test from './functional'
+import Test from './functional';
+import Classtest from './classf'
 
 function App() {
   return (
     <div className="App">
       <h1>I am Learning React</h1>
       <Test name="Ashique Abdullah"></Test>
+      <Classtest age="24"></Classtest>
     </div>
   );
 }
---
+++ another react/learnreact/src/component/classf.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react'
+
+class Classtest extends Component{
+    render(){
+        return <h2>I am {this.props.age} years old</h2>
+        
+    }
+}
+export default Classtest
