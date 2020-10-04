85fa17a8c575a55ddd132e013620aad8fbaca7d5
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-10-04 13:53:59 +0600

    JSX Syntax

--- learn/src/App.js
+++ learn/src/App.js
@@ -2,20 +2,23 @@ import React from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div>
-      <h1>My Name is:</h1>
-      <Name />
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <h1>My Name is:</h1>
+      
+  //   </div>
+  // );
+
+  return React.createElement('div',null,React.createElement('h1',null,'My name Is:'),<Name />);
 }
 
 function Name() {
-  return (
-    <div>
-      <h2>Ashique Abdullah</h2>
-    </div>
-  );
+  // return (
+  //   <div>
+  //     <h2>Ashique Abdullah</h2>
+  //   </div>
+  // );
+  return React.createElement('div',null,React.createElement('h1',null,'Md: Ashique Abdullah'));
 }
 
   
