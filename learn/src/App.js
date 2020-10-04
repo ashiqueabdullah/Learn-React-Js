9e1644053c7303c4d96ea58a2c33748c5e5a98d2
Ashique Abdullah <ashiqueashique465@gmail.com>
2020-10-04 13:03:41 +0600

    React Component Basic
    The first alphabet of the function must use a capital letter

---
+++ learn/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>My Name is:</h1>
+      <Name />
+    </div>
+  );
+}
+
+function Name() {
+  return (
+    <div>
+      <h2>Ashique Abdullah</h2>
+    </div>
+  );
+}
+
+  
+
+
+export default App;
---
+++ learn/src/component/allcomponent.js
@@ -0,0 +1,10 @@
+
+function allcomponent() {
+    return(
+        <div>
+            <h1>Ashique Abdullah</h1>
+        </div>
+    );
+}
+
+export default allcomponent;
